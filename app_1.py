029a6c80e2d73d0cb30876d5109c3b8b9b57e38c
Bishwajit Karmaker <bish.karm123@gmail.com>
2023-02-14 14:38:18 +0100

    python, string & print function

---
+++ app_1.py
@@ -0,0 +1,14 @@
+name = 'Bishwajit'
+age = 18
+print('Hello from Python world.')
+print('Welcome',  name.upper())
+print('Your age is', age)
+print(name.lower(),'How are you? \nHow are you feeling today?')
+print(len(name))
+print(name.index('j'))
+print(name.replace('j', 'g'))
+
+
+
+
+print(78)
