f23db83bc4b06fdafa34d6bc490192877f57bf4f
Bishwajit Karmaker <bish.karm123@gmail.com>
2023-02-18 00:32:41 +0100

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+my_dict = {
+    'name': 'Tim',
+    'nationality': 'Bangladeshi',
+    'age': 30,
+    'is_short': True,
+    'qualification': 'bachelor'
+} # duplicates are not allowed in dictionary
+print(my_dict)
+print(my_dict['name'])
+print(len(my_dict))
