c0af3f52dd71b493a4978ec0da30facb42c6c132
Bishwajit Karmaker <bish.karm123@gmail.com>
2023-02-18 00:24:13 +0100

    if_else_statement

---
+++ if_else_statement.py
@@ -0,0 +1,8 @@
+a = input('Please enter a number: ')
+b = input('Please enter another number: ')
+if a > b:
+    print(a, 'is greater than', b)
+elif a == b:
+    print(a, 'is equal to', b)
+else:
+    print(a, 'is smaller than',b)
