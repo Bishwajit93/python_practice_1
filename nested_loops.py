7ce4a1700d89369e594dd0d9e62caec6ba884748
Bishwajit Karmaker <bish.karm123@gmail.com>
2023-02-18 01:00:56 +0100

    nested_loops

---
+++ nested_loops.py
@@ -0,0 +1,13 @@
+my_list = [
+    [1,2,3,4],
+    [4,5,6,7],
+    [8,9,10,11]
+]
+
+# print(my_list)
+# print(my_list[0])
+# print(my_list[0][1])
+
+for list in my_list:
+    for row in list:
+        print(row)
