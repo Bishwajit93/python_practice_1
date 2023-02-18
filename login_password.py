c0bc04d15ad9ce5a5ffd68028b47347aec62c82c
Bishwajit Karmaker <bish.karm123@gmail.com>
2023-02-18 02:06:39 +0100

    account_password

---
+++ login_password.py
@@ -0,0 +1,14 @@
+print('Create an account')
+username = input('Enter username: ')
+password = input('Enter password: ')
+
+print('Your account has been created successfully!!!!!!!!!')
+print('Login now!')
+
+username1 = input('Enter your username: ')
+password1 = input('Enter your password: ')
+
+if username == username1 and password == password1:
+    print('Logged in Succesfully')
+else:
+    print('Invalid credentials')
